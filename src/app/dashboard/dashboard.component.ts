e0cd7bd2a37ed6dee101aa62ff3c15b8d45518b5
Allison Schambers <aschambers4291@gmail.com>
2023-11-17 18:42:43 -0800

    Use sprites from pokemon showdown

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Match } from './match.model';
 import { Player } from './player.model';
+import { ReplayService } from './replay.service';
+import { Sprites } from '@pkmn/img';
 
 @Component({
   selector: 'app-dashboard',
@@ -10,68 +12,55 @@ import { Player } from './player.model';
 export class DashboardComponent {
   match: Match | undefined;
   player: Player | undefined;
-  files: Array<string> = [];
   replays: Array<Match> = [];
   filteredReplays: Array<Match> = [];
   pokemonList: Array<string> = [];
-  username: string = '';
-  displayHtml: string = '';
-  pokemonListMap: any = {};
+  sortedPokemon: Array<Array<string | number>> = [];
   playerList: Array<string> = [];
   playerRatingList: Array<Player> = [];
   ledPokemonList: Array<string> = [];
-  filteredLedPokemonList: Array<string> = [];
-  uniquePokemon: Array<string> = [];
-  filteredUniquePokemon: Array<string> = [];
   searchType: string = 'ledWith';
   viewStats: boolean = false;
-  sortedPokemon: Array<Array<string | number>> = [];
 
-  viewPokemonStats = () => {
-    this.viewStats = !this.viewStats;
+  constructor(private replayService: ReplayService) {}
+
+  getSprite = (pokemon: string) => {
+    const { url } = Sprites.getPokemon(pokemon, { gen: 'gen5' });
+    return url;
   }
 
-  findStringIndex = (str1: string, str2: string) => {
-    const output = [];
-    for (let i = 0; i < str1.length -1; i++) {
-      let arr = [];
-      for (let j = i; j < (i+str2.length) && i < str1.length; j++) {
-        arr.push(str1[j]);
+  /**
+   * triggers the parseBattleData function when files are uploaded
+   * @param event Event
+   */
+  fileChanged = (event: Event) => {
+    const files = (event.target as any).files;
+    for (let i = 0; i < files.length; i++) {
+      if (files[i].type === 'text/html') {
+        const reader = new FileReader();
+        reader.readAsText(files[i], "UTF-8");
+        reader.onload = (event: Event) => {
+          const battleData = (event.target as any).result;
+          const baseUrl = `C://Users//allis//`;
+          const relativePath = files[i].webkitRelativePath;
+          const fullUrl = baseUrl + relativePath;
+          this.parseBattleData(battleData, fullUrl);
+        }
+        reader.onerror = () => {
+          console.log('error reading file');
+        }
       }
-
-      const findnew = arr.join('');
-      if (findnew === str2) {
-        output.push(i);
-      }
     }
-    return output;
   }
 
-  getPokemonNameString = (pokemonData: string) => {
-    if (!pokemonData) {
-      return '';
-    }
-
-    const startIndex = pokemonData.indexOf('|poke') + 9;
-    const endIndex = pokemonData.indexOf(',');
-    return pokemonData.slice(startIndex, endIndex).replace(' ', '').replace('-*', '').toLowerCase();
-  }
-
-  getPokemonSwitchString = (pokemonData: string) => {
-    if (!pokemonData) {
-      return '';
-    }
-
-    const startIndex = this.getPosition(pokemonData, '|', 3) + 1;
-    const endIndex = pokemonData.indexOf(',');
-    return pokemonData.slice(startIndex, endIndex).replace(' ', '').replace('-*', '').toLowerCase().replace('urshifu-rapid-strike', 'urshifu');
-  }
-
-  getPosition = (string: string, substring: string, index: number) => {
-    return string.split(substring, index).join(substring).length;
-  }
-
-  parseBattleData(battleData: string, index: number, fullUrl: string) {
+  /**
+   * parse battle data to be displayed on the page
+   * for the user to visualize
+   * @param battleData string
+   * @param fullUrl string
+   */
+  parseBattleData = (battleData: string, fullUrl: string) => {
+    // player array
     const allPokemonUsedInMatch = [];
     const startPlayer1 = battleData.indexOf('|player|p1|');
     const endPlayer1 = battleData.indexOf('|p1') + 4;
@@ -81,6 +70,7 @@ export class DashboardComponent {
       this.playerList.push(player1);
     }
 
+    // player array
     const startPlayer2 = battleData.indexOf('|player|p2|');
     const endPlayer2 = battleData.indexOf('|p2') + 4;
     const endPlayer2Name = endPlayer2 + battleData.substring(endPlayer2).indexOf('|') + 1;
@@ -89,13 +79,14 @@ export class DashboardComponent {
       this.playerList.push(player2);
     }
 
+    // rating of players array
     const startRatingPlayer1 = startPlayer2 - 5;
     const endRatingPlayer1 = startPlayer2;
     const ratingPlayer1 = battleData.slice(startRatingPlayer1, endRatingPlayer1).replace('\n', '');
-    
     const endRatingPlayer2 = battleData.indexOf('|teamsize|p1');
     const startRatingPlayer2 = endRatingPlayer2 - 5;
     const ratingPlayer2 = battleData.slice(startRatingPlayer2, endRatingPlayer2).replace('\n', '');
+
     if (this.playerRatingList) {
       this.playerRatingList.push({
         player: player1,
@@ -111,73 +102,74 @@ export class DashboardComponent {
     const pokemonTagPlayer1 = '|poke|p1';
     const pokemonTagPlayer2 = '|poke|p2';
     const teamPreviewIndex = battleData.indexOf('|teampreview');
-    const pokemonIndexesPlayer1 = this.findStringIndex(battleData, pokemonTagPlayer1) || [];
-    const pokemonIndexesPlayer2 = this.findStringIndex(battleData, pokemonTagPlayer2) || [];
+    const pokemonIndexesPlayer1 = this.replayService.findStringIndex(battleData, pokemonTagPlayer1) || [];
+    const pokemonIndexesPlayer2 = this.replayService.findStringIndex(battleData, pokemonTagPlayer2) || [];
 
     // player1 pokemon
     const firstPokePlayer1string = battleData.slice(pokemonIndexesPlayer1[0], pokemonIndexesPlayer1[1]);
-    const player1pokemon1 = this.getPokemonNameString(firstPokePlayer1string);
+    const player1pokemon1 = this.replayService.getPokemonNameString(firstPokePlayer1string);
     allPokemonUsedInMatch.push(player1pokemon1);
     const secondPokePlayer1string = battleData.slice(pokemonIndexesPlayer1[1], pokemonIndexesPlayer1[2]);
-    const player1pokemon2 = this.getPokemonNameString(secondPokePlayer1string);
+    const player1pokemon2 = this.replayService.getPokemonNameString(secondPokePlayer1string);
     allPokemonUsedInMatch.push(player1pokemon2);
     const thirdPokePlayer1string = battleData.slice(pokemonIndexesPlayer1[2], pokemonIndexesPlayer1[3]);
-    const player1pokemon3 = this.getPokemonNameString(thirdPokePlayer1string);
+    const player1pokemon3 = this.replayService.getPokemonNameString(thirdPokePlayer1string);
     allPokemonUsedInMatch.push(player1pokemon3);
     const fourthPokePlayer1string = battleData.slice(pokemonIndexesPlayer1[3], pokemonIndexesPlayer1[4]);
-    const player1pokemon4 = this.getPokemonNameString(fourthPokePlayer1string);
+    const player1pokemon4 = this.replayService.getPokemonNameString(fourthPokePlayer1string);
     allPokemonUsedInMatch.push(player1pokemon4);
     const fifthPokePlayer1string = battleData.slice(pokemonIndexesPlayer1[4], pokemonIndexesPlayer1[5]);
-    const player1pokemon5 = this.getPokemonNameString(fifthPokePlayer1string);
+    const player1pokemon5 = this.replayService.getPokemonNameString(fifthPokePlayer1string);
     allPokemonUsedInMatch.push(player1pokemon5);
     const sixthPokePlayer1string = battleData.slice(pokemonIndexesPlayer1[5], pokemonIndexesPlayer2[0]);
-    const player1pokemon6 = this.getPokemonNameString(sixthPokePlayer1string);
+    const player1pokemon6 = this.replayService.getPokemonNameString(sixthPokePlayer1string);
     allPokemonUsedInMatch.push(player1pokemon6);
 
     // player2 pokemon
     const firstPokePlayer2string = battleData.slice(pokemonIndexesPlayer2[0], pokemonIndexesPlayer2[1]);
-    const player2pokemon1 = this.getPokemonNameString(firstPokePlayer2string);
+    const player2pokemon1 = this.replayService.getPokemonNameString(firstPokePlayer2string);
     allPokemonUsedInMatch.push(player2pokemon1);
     const secondPokePlayer2string = battleData.slice(pokemonIndexesPlayer2[1], pokemonIndexesPlayer2[2]);
-    const player2pokemon2 = this.getPokemonNameString(secondPokePlayer2string);
+    const player2pokemon2 = this.replayService.getPokemonNameString(secondPokePlayer2string);
     allPokemonUsedInMatch.push(player2pokemon2);
     const thirdPokePlayer2string = battleData.slice(pokemonIndexesPlayer2[2], pokemonIndexesPlayer2[3]);
-    const player2pokemon3 = this.getPokemonNameString(thirdPokePlayer2string);
+    const player2pokemon3 = this.replayService.getPokemonNameString(thirdPokePlayer2string);
     allPokemonUsedInMatch.push(player2pokemon3);
     const fourthPokePlayer2string = battleData.slice(pokemonIndexesPlayer2[3], pokemonIndexesPlayer2[4]);
-    const player2pokemon4 = this.getPokemonNameString(fourthPokePlayer2string);
+    const player2pokemon4 = this.replayService.getPokemonNameString(fourthPokePlayer2string);
     allPokemonUsedInMatch.push(player2pokemon4);
     const fifthPokePlayer2string = battleData.slice(pokemonIndexesPlayer2[4], pokemonIndexesPlayer2[5]);
-    const player2pokemon5 = this.getPokemonNameString(fifthPokePlayer2string);
+    const player2pokemon5 = this.replayService.getPokemonNameString(fifthPokePlayer2string);
     allPokemonUsedInMatch.push(player2pokemon5);
     const sixthPokePlayer2string = battleData.slice(pokemonIndexesPlayer2[5], teamPreviewIndex);
-    const player2pokemon6 = this.getPokemonNameString(sixthPokePlayer2string);
+    const player2pokemon6 = this.replayService.getPokemonNameString(sixthPokePlayer2string);
     allPokemonUsedInMatch.push(player2pokemon6);
 
     // player1 lead pokemon
     const player1lead1string = '|switch|p1';
     const player2lead1string = '|switch|p2';
-    const pokemonLeadIndexesPlayer1 = this.findStringIndex(battleData, player1lead1string);
-    const pokemonLeadIndexesPlayer2 = this.findStringIndex(battleData, player2lead1string);
+    const pokemonLeadIndexesPlayer1 = this.replayService.findStringIndex(battleData, player1lead1string);
+    const pokemonLeadIndexesPlayer2 = this.replayService.findStringIndex(battleData, player2lead1string);
 
+    // led pokemon array
     const firstSelectedPokemonPlayer1string = battleData.slice(pokemonLeadIndexesPlayer1[0], pokemonLeadIndexesPlayer1[1]);
-    const player1lead1 = this.getPokemonSwitchString(firstSelectedPokemonPlayer1string);
+    const player1lead1 = this.replayService.getPokemonSwitchString(firstSelectedPokemonPlayer1string);
     if (!this.ledPokemonList.includes(player1lead1)) {
       this.ledPokemonList.push(player1lead1);
     }
     const secondSelectedPokemonPlayer1string = battleData.slice(pokemonLeadIndexesPlayer1[1], pokemonLeadIndexesPlayer1[2]);
-    const player1lead2 = this.getPokemonSwitchString(secondSelectedPokemonPlayer1string);
+    const player1lead2 = this.replayService.getPokemonSwitchString(secondSelectedPokemonPlayer1string);
     if (!this.ledPokemonList.includes(player1lead2)) {
       this.ledPokemonList.push(player1lead2);
     }
 
     const firstSelectedPokemonPlayer2string = battleData.slice(pokemonLeadIndexesPlayer2[0], pokemonLeadIndexesPlayer2[1]);
-    const player2lead1 = this.getPokemonSwitchString(firstSelectedPokemonPlayer2string);
+    const player2lead1 = this.replayService.getPokemonSwitchString(firstSelectedPokemonPlayer2string);
     if (!this.ledPokemonList.includes(player2lead1)) {
       this.ledPokemonList.push(player2lead1);
     }
     const secondSelectedPokemonPlayer2string = battleData.slice(pokemonLeadIndexesPlayer2[1], pokemonLeadIndexesPlayer2[2]);
-    const player2lead2 = this.getPokemonSwitchString(secondSelectedPokemonPlayer2string);
+    const player2lead2 = this.replayService.getPokemonSwitchString(secondSelectedPokemonPlayer2string);
     if (!this.ledPokemonList.includes(player2lead2)) {
       this.ledPokemonList.push(player2lead2);
     }
@@ -216,100 +208,34 @@ export class DashboardComponent {
       replayLink: fullUrl
     }
 
-    this.sortedPokemon = this.sortPokemon(allPokemonUsedInMatch);
+    this.sortedPokemon = this.replayService.sortPokemon(allPokemonUsedInMatch);
     this.replays.push(matchInfo);
     this.filteredReplays.push(matchInfo);
-    console.log(this.sortedPokemon);
   }
 
-  sortPokemon = (allPokemonUsedInMatch: Array<string>) => {
-    for (let i = 0; i < allPokemonUsedInMatch.length; i++) {
-      if (this.pokemonListMap[allPokemonUsedInMatch[i]] === undefined) {
-        this.pokemonListMap[allPokemonUsedInMatch[i]] = 1;
-      } else {
-        this.pokemonListMap[allPokemonUsedInMatch[i]] += 1;
-      }
-    }
-    let sortable = [];
-    for (var pokemon in this.pokemonListMap) {
-        sortable.push([pokemon, this.pokemonListMap[pokemon]]);
-        if (!this.uniquePokemon.includes(pokemon)) {
-          this.uniquePokemon.push(pokemon);
-        }
-    }
-
-    console.log(this.pokemonListMap);
-
-    sortable.sort(function(a, b) {
-        return b[1] - a[1];
-    });
-
-    return sortable;
+  /**
+   * toggles viewable pokemon stats
+   */
+  viewPokemonStats = () => {
+    this.viewStats = !this.viewStats;
   }
 
-  fileChanged(event: Event) {
-    const files = (event.target as any).files;
-    for (let i = 0; i < files.length; i++) {
-      if (files[i].type === 'text/html') {
-        const reader = new FileReader();
-        reader.readAsText(files[i], "UTF-8");
-        reader.onload = (event: Event) => {
-          const battleData = (event.target as any).result;
-          const baseUrl = `C://Users//allis//`;
-          const relativePath = files[i].webkitRelativePath;
-          const fullUrl = baseUrl + relativePath;
-          this.parseBattleData(battleData, i, fullUrl);
-        }
-        reader.onerror = () => {
-          console.log('error reading file');
-        }
-      }
+  /**
+   * selects the type of search to perform
+   * @param event Event
+   */
+  selectSearchType = (event: Event) => {
+    this.searchType = (event.target as any).value;
+    this.filteredReplays.length = 0;
+    for (let i = 0; i < this.replays.length; i++) {
+      this.filteredReplays.push(this.replays[i]);
     }
   }
 
-  getPokemonBackground = (replay: Match, player1pokemon1: string, player1lead1: string, player1lead2: string,
-  player2pokemon1: string, player2lead1: string, player2lead2: string) => {
-    const winner = replay.winner;
-    const player1 = replay.player1;
-    const player2 = replay.player2;
-
-    if (winner === player1) {
-      if (player1pokemon1 === player1lead1 || player1pokemon1 === player1lead2) {
-        return 'winnerselected';
-      } else if (player1pokemon1 !== player1lead1 && player1pokemon1 !== player1lead2 && player2pokemon1 === '') {
-        return 'winnerunselected';
-      } else if (player2pokemon1 === player2lead1 || player2pokemon1 === player2lead2) {
-        return 'loserselected';
-      } else if (player2pokemon1 !== player2lead1 && player2pokemon1 !== player2lead2 && player1pokemon1 === '') {
-        return 'loserunselected';
-      } else {
-        return '';
-      }
-    } else if (winner === player2) {
-      if (player2pokemon1 === player2lead1 || player2pokemon1 === player2lead2) {
-        return 'winnerselected';
-      } else if (player2pokemon1 !== player2lead1 && player2pokemon1 !== player2lead2 && player1pokemon1 === '') {
-        return 'winnerunselected';
-      } else if (player1pokemon1 === player1lead1 || player1pokemon1 === player1lead2) {
-        return 'loserselected';
-      } else if (player1pokemon1 !== player1lead1 && player1pokemon1 !== player1lead2) {
-        return 'loserunselected';
-      } else {
-        return '';
-      }
-    } else {
-      return '';
-    }
-  }
-
-  viewReplay = (replayLink: string) => {
-    window.open(replayLink);
-  }
-
-  filterReplays = (searchValue: string) => {
-    this.filteredReplays = this.replays.filter(replay => replay.player1.toLowerCase().includes(searchValue.toLowerCase()));
-  }
-
+  /**
+   * filters the array of pokemon used
+   * @param searchValue string
+   */
   filterPokemonLed = (searchValue: string) => {
     const replays = this.replays;
 
@@ -322,6 +248,10 @@ export class DashboardComponent {
     }
   }
 
+  /**
+   * filters the array of pokemon led
+   * @param searchValue string
+   */
   filterPokemonBrought = (searchValue: string) => {
     const replays = this.replays;
 
@@ -338,11 +268,19 @@ export class DashboardComponent {
     }
   }
 
-  selectSearchType = (event: Event) => {
-    this.searchType = (event.target as any).value;
-    this.filteredReplays.length = 0;
-    for (let i = 0; i < this.replays.length; i++) {
-      this.filteredReplays.push(this.replays[i]);
-    }
+  /**
+   * returns a string to determine what background to apply to pokemon being displayed on the page
+   * @param replay Match
+   * @param player1pokemon1 string
+   * @param player1lead1 string
+   * @param player1lead2 string
+   * @param player2pokemon1 string
+   * @param player2lead1 string
+   * @param player2lead2 string
+   * @returns string
+   */
+  getPokemonBackground = (replay: Match, player1pokemon1: string, player1lead1: string, player1lead2: string,
+  player2pokemon1: string, player2lead1: string, player2lead2: string) => {
+    return this.replayService.getPokemonBackground(replay, player1pokemon1, player1lead1, player1lead2, player2pokemon1, player2lead1, player2lead2)
   }
 }
--- src/app/dashboard/replay.service.ts
+++ src/app/dashboard/replay.service.ts
@@ -1,9 +1,149 @@
 import { Injectable } from '@angular/core';
+import { Match } from './match.model';
+import { Player } from './player.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ReplayService {
+  match: Match | undefined;
+  player: Player | undefined;
+  replays: Array<Match> = [];
+  filteredReplays: Array<Match> = [];
+  pokemonList: Array<string> = [];
+  pokemonListMap: any = {};
+  
+  /**
+   * returns an array with two indexes for desired string
+   * @param str1 string
+   * @param str2 string
+   * @returns array
+   */
+  findStringIndex = (str1: string, str2: string) => {
+    const output = [];
+    for (let i = 0; i < str1.length -1; i++) {
+      let arr = [];
+      for (let j = i; j < (i+str2.length) && i < str1.length; j++) {
+        arr.push(str1[j]);
+      }
 
-  constructor() { }
+      const findnew = arr.join('');
+      if (findnew === str2) {
+        output.push(i);
+      }
+    }
+    return output;
+  }
+
+  /**
+   * returns the name of the pokemon based on the index of specified string
+   * @param pokemonData string
+   * @returns string
+   */
+  getPokemonNameString = (pokemonData: string) => {
+    if (!pokemonData) {
+      return '';
+    }
+
+    const startIndex = pokemonData.indexOf('|poke') + 9;
+    const endIndex = pokemonData.indexOf(',');
+    return pokemonData.slice(startIndex, endIndex).replace(' ', '').replace('-*', '').toLowerCase();
+  }
+
+  /**
+   * returns the name of the switched in
+   * @param pokemonData string
+   * @returns string
+   */
+  getPokemonSwitchString = (pokemonData: string) => {
+    if (!pokemonData) {
+      return '';
+    }
+
+    const startIndex = this.getPosition(pokemonData, '|', 3) + 1;
+    const endIndex = pokemonData.indexOf(',');
+    return pokemonData.slice(startIndex, endIndex).replace(' ', '').toLowerCase();
+  }
+
+  /**
+   * returns the position of a string
+   * @param string string
+   * @param substring string
+   * @param index number
+   * @returns number
+   */
+  getPosition = (string: string, substring: string, index: number) => {
+    return string.split(substring, index).join(substring).length;
+  }
+
+  /**
+   * returns an array of sorted pokemon
+   * @param allPokemonUsedInMatch Array<string>
+   * @returns Array
+   */
+  sortPokemon = (allPokemonUsedInMatch: Array<string>) => {
+    for (let i = 0; i < allPokemonUsedInMatch.length; i++) {
+      if (this.pokemonListMap[allPokemonUsedInMatch[i]] === undefined) {
+        this.pokemonListMap[allPokemonUsedInMatch[i]] = 1;
+      } else {
+        this.pokemonListMap[allPokemonUsedInMatch[i]] += 1;
+      }
+    }
+    let sortable = [];
+    for (var pokemon in this.pokemonListMap) {
+      sortable.push([pokemon, this.pokemonListMap[pokemon]]);
+    }
+
+    sortable.sort((a, b) => {
+      return b[1] - a[1];
+    });
+
+    return sortable;
+  }
+
+  /**
+   * returns a string to determine what background to apply to pokemon being displayed on the page
+   * @param replay Match
+   * @param player1pokemon1 string
+   * @param player1lead1 string
+   * @param player1lead2 string
+   * @param player2pokemon1 string
+   * @param player2lead1 string
+   * @param player2lead2 string
+   * @returns string
+   */
+  getPokemonBackground = (replay: Match, player1pokemon1: string, player1lead1: string, player1lead2: string,
+  player2pokemon1: string, player2lead1: string, player2lead2: string) => {
+    const winner = replay.winner;
+    const player1 = replay.player1;
+    const player2 = replay.player2;
+
+    if (winner === player1) {
+      if (player1pokemon1 === player1lead1 || player1pokemon1 === player1lead2) {
+        return 'winnerselected';
+      } else if (player1pokemon1 !== player1lead1 && player1pokemon1 !== player1lead2 && player2pokemon1 === '') {
+        return 'winnerunselected';
+      } else if (player2pokemon1 === player2lead1 || player2pokemon1 === player2lead2) {
+        return 'loserselected';
+      } else if (player2pokemon1 !== player2lead1 && player2pokemon1 !== player2lead2 && player1pokemon1 === '') {
+        return 'loserunselected';
+      } else {
+        return '';
+      }
+    } else if (winner === player2) {
+      if (player2pokemon1 === player2lead1 || player2pokemon1 === player2lead2) {
+        return 'winnerselected';
+      } else if (player2pokemon1 !== player2lead1 && player2pokemon1 !== player2lead2 && player1pokemon1 === '') {
+        return 'winnerunselected';
+      } else if (player1pokemon1 === player1lead1 || player1pokemon1 === player1lead2) {
+        return 'loserselected';
+      } else if (player1pokemon1 !== player1lead1 && player1pokemon1 !== player1lead2) {
+        return 'loserunselected';
+      } else {
+        return '';
+      }
+    } else {
+      return '';
+    }
+  }
 }
